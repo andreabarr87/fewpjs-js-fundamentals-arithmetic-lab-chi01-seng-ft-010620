9163099576305b8686975715ccbe672257d2fd45
Andrea <andreabarr87@gmail.com>
2020-02-25 10:04:59 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 2
+let num2 = 31
+let val1 = 44
+let val2 = 5
 
-// Write your code here
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 10) + 1
+const mod = val1 % val2
+const max = Math.max(1, 5, 10, 20)
